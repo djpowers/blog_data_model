932a725da05231ddfb852e3e7736902fcfa29eeb
Dave Powers <djpowers89@gmail.com>
2013-12-17 11:23:55 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DesignBlogDataModel::Application.config.secret_key_base = '0fb61c6590cb2960d74c51fd90e5788d2103cedd8a0d7f9fe8f510f2c618fae5daf478f3ae38340aea5994782521f07380a4ba6bb0eac9b56d62cfdb90a373b7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DesignBlogDataModel::Application.config.session_store :cookie_store, key: '_design_blog_data_model_session'
